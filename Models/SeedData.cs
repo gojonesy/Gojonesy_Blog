89b3bf97836e71e3d1d82958a203f8f5871c860d
Justin Jones <jones.justin@mhsil.com>
2018-04-03 13:21:39 -0500

    Added DB and SeedData

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -5,5 +5,34 @@ using System.Linq;
 
 namespace Gojonesy_Blog.Models
 {
-    
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BlogContext(serviceProvider.GetRequiredService<DbContextOptions<BlogContext>>()))
+                {
+                    if (context.Blog.Any())
+                    {
+                        return; // DB is already seeded.
+                    }
+
+                    context.Blog.AddRange(
+                        new Blog
+                        {
+                            Title = "Welcome to my blog",
+                            PostDate = DateTime.Parse("2018-03-29"),
+                            PostBody = "Welcome to my new blog. Here is some Bacon ipsum.\n Bacon ipsum dolor amet landjaeger strip steak jerky chicken brisket, \ntri - tip porchetta.Ham capicola ribeye boudin tail pork.Shank tenderloin pastrami kielbasa, \nswine drumstick sirloin alcatra hamburger prosciutto.Buffalo alcatra jowl, \nground round porchetta ball tip t - bone short ribs.Beef burgdoggen shoulder kevin alcatra pork loin tenderloin.Short loin pork chop flank, \ndoner swine shoulder tenderloin venison sirloin fatback salami porchetta short ribs."
+                        }, 
+                        new Blog
+                        {
+                            Title = "My second post.",
+                            PostDate = DateTime.Parse("2018-04-02"),
+                            PostBody = "Welcome to my second post.. Here is some Bacon ipsum.\n Bacon ipsum dolor amet landjaeger strip steak jerky chicken brisket, \ntri - tip porchetta.Ham capicola ribeye boudin tail pork.Shank tenderloin pastrami kielbasa, \nswine drumstick sirloin alcatra hamburger prosciutto.Buffalo alcatra jowl, \nground round porchetta ball tip t - bone short ribs.Beef burgdoggen shoulder kevin alcatra pork loin tenderloin.Short loin pork chop flank, \ndoner swine shoulder tenderloin venison sirloin fatback salami porchetta short ribs."
+                        }
+                    );
+
+                    context.SaveChanges();
+                }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Gojonesy_Blog.Models;
 
 namespace Gojonesy_Blog
 {
@@ -14,7 +16,25 @@ namespace Gojonesy_Blog
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires models
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
